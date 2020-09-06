65fc5d73344dad6c5242b769830f159be87b96fd
maneletorres <maneletorres@hotmail.com>
2020-09-06 16:13:34 +0200

    Added necessary import.

--- lab-8-client/src/main/java/com/maneletorres/webapp/LuckyWordController.java
+++ lab-8-client/src/main/java/com/maneletorres/webapp/LuckyWordController.java
@@ -1,6 +1,6 @@
 package com.maneletorres.webapp;
 
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
